a4b106bf9d92ac3fdb7393b6c2ee03f29775c130
Chloe Atwood <atwoodksw@gmail.com>
2020-10-21 03:45:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -17,7 +17,7 @@ end
 
 # define method with three paramaters
 def move(board, input, xo = "X")
-# update board array with user_input index equal to string
+# update board array with index equal to string
   board[input] = xo
 # end method definition
 end
