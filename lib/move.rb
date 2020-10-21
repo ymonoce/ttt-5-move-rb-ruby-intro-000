7110c1693a8534cf99bf4e2e848b81437a6e2154
Chloe Atwood <atwoodksw@gmail.com>
2020-10-21 03:29:17 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,7 @@ end
 # define method with one paramater
 def input_to_index(user_input)
 # converts argument to integer and subracts one
-user_input.to_i - 1
+input.to_i - 1
 # end method definition
 end
 
