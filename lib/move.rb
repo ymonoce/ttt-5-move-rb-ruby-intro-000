18e4043ef427b58e310e4fd5f19cc26216083801
Chloe Atwood <atwoodksw@gmail.com>
2020-10-21 02:48:49 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -19,6 +19,6 @@ end
 def move(board, user_input, xo = "X")
   input_to_index("#{user_input}")
 # update board array with user_input index equal to string
-  board["#{user_input}"] = xo
+  board[user_input] = xo
 # end method definition
 end
