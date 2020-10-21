5274866c9fc74a1b461aef46544802646bd92c8b
Chloe Atwood <atwoodksw@gmail.com>
2020-10-21 03:53:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -4,7 +4,7 @@ def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
 # puts string representing border of board
   puts "-----------"
-# puts top row of board, three cells mapped to array index
+# ibid
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
 # ibid
   puts "-----------"
