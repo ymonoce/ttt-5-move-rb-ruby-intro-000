f9ffe0dc6d0ea7f865d867778fc0720edd66bbd4
Chloe Atwood <atwoodksw@gmail.com>
2020-10-21 02:33:26 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,4 +1,6 @@
+# define method with one argument
 def display_board(board)
+# top row of board, three cells of three empty spaces
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
@@ -6,4 +8,16 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+# define method with one paramater
+def input_to_index(input)
+# converts argument to integer and subracts one
+index = input.to_i - 1
+# end method definition
+end
+
+# define method with three paramaters
+def move(board, index, xo = "X")
+# update board array with user_input index equal to string
+  board[index] = xo
+# end method definition
+end
